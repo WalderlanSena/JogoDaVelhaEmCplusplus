9f7e6f10ba78958dcc7bc5464b08e694d0d2430a
Walderlan Sena <senawalderlan3@gmail.com>
2017-02-12 23:07:42 -0300

    Modificação do menu de opções, adicionada a função de modo singleplayer e multplayer com a posibilidade de escolha dos nomes do jogadores. Adicionado novas orientações no README, e a opção de singleplayer ainda não foi desenvolvida. Todavia o codigo foi otimizado e corrigidos alguns bugs...

--- README.md
+++ README.md
@@ -2,11 +2,27 @@
 
 Jogo da Velha em C++
 
+
+#Exemplo do Funcionamento dos Arrays(Tabelas)
+
+```
+	Quando o usuário adiciona um X ou O na posição respectiva da tabela, outra tabela em
+	background guarda este valor para que seja impossivel sua remarcação
+```
+![image](https://github.com/WalderlanSena/jogodavelhaemc/blob/master/img/array.png)
+
+=============================================================
+
 #Exemplo do Software
 
-![image](https://github.com/WalderlanSena/jogodavelhaemc/blob/master/img/exemplo.png)
+![image](https://github.com/WalderlanSena/jogodavelhaemc/blob/master/img/exemplo.jpg)
 
 =============================================================
 ```
 	Digite uma posição, ultilizando as teclas de 1 a 9.
+```
+# Como compilar ? Obs: No caso de SO Windows altere a linha 15 (No Define) para CLS !
+
+```
+	g++ jogodavelha.cpp -o jogodavelha
 ```
--- jogodavelha.cpp
+++ jogodavelha.cpp
@@ -1,8 +1,10 @@
-//----------------------------------------//
-// JOGO DA VELHA - em C++                //
-// Desenvolvedor: Walderlan Soares Sena //
-//                                     //
-// -----------------------------------//
+// ############################################################
+// 										Jogo da Velha em C++
+//                									v0.2.3
+//										Software Open Source
+// Desenvolvedor: Walderlan Sena | by: http://www.walderlan.xyz
+// Curriculo: https://github.com/WalderlanSena/curriculo
+// ############################################################
 
 #include <iostream>
 #include <iomanip>
@@ -10,88 +12,130 @@
 
 using namespace std;
 
-int  i;//Controle do Laço
-char tabela[3][3] = {'1','2','3','4','5','6','7','8','9'};//Array da tabela
-int  control[3][3];
-bool result;//Armazena Resultado das Posições
-int  jogadas = 0;//Calcula numeros de Jogadas
+#define LIMPAR "clear" // Difine para Limpar a tela [ Modifique para 'cls' no caso de Windows ]
 
-bool resultado(){
-	//Verifica a tabela no modo horizontal
-	if(tabela[0][0] == 'O' && tabela[0][1] == 'O' && tabela[0][2] == 'O' || 
+int  i;  //Controle do Laço
+char tabela[3][3]   = {'1','2','3','4','5','6','7','8','9'}; //Array da tabela
+int  control[3][3];                  // Array de controle de jogadas dos usuarios
+int  jogadas        = 0;            // Calcula numeros de Jogadas
+int  modo           = 0;           // Modo do jogo inicializado em SinglePlayer ou MultiPlayer
+char player1[50]    = "";  // Nome do 1° jogador(a)
+char player2[50]    = ""; //  Nome do 2° jogador(a)
+
+// Função de demostração do nome e informações da versão do Jogo
+void menuBanner(){
+	system("clear");
+	cout << "x-----------------------------x" << endl;
+	cout << "o    JOGO DA VELHA EM C++     o" << endl;
+	cout << "o                   v0.2.3    o" << endl;
+	cout << "x-----------------------------x" << endl;
+	cout << endl;
+}
+// Funçõa de escolha para o modo do jogo - Single Player ou MultiPlayer
+void menuModo(){
+	menuBanner();
+	cout << setw(27) << "SELECIONE O MODO DO JOGO:" << endl << endl;
+	cout << setw(25) << "[ 1  - Single Player ]"    << endl;
+	cout << setw(25) << "[ 2  - MultiPlayer   ]"    << endl;
+	cout << setw(25) << "[ 99 - Sair do Jogo  ]"    << endl;
+	cout << setw(25) << "--------------------"    << endl;
+	cout << setw(27) << "Digite o Modo do Jogo > ";
+		cin >> modo; // atribuindo o valor ao modo do Jogo
+		// Verificando qual opção foi escolhida
+		switch(modo){
+			case 1:
+				cout << "Desculpe ! Opcao ainda em desenvolvimento..." << endl;
+				exit(0);
+				break;
+
+			case 2:
+				cout << "Digite o nome do 1° Jogador(a) sem espacos: ";
+					cin >> player1;
+				cout << "Digite o nome do 2° Jogador(a) sem espacos: ";
+					cin >> player2;
+				break;
+
+			case 99:
+				cout << endl << "#------------- Fim de Jogo -------------#" << endl;
+				cout << "#------------------ by: Walderlan Sena -#" << endl;
+				exit(0);
+				break;
+
+			default:
+				exit(0);
+		} // end switch
+
+}
+
+char resultado(char player1[],char player2[]){
+	// Verifica a tabela no modo horizontal
+	if(tabela[0][0] == 'O' && tabela[0][1] == 'O' && tabela[0][2] == 'O' ||
 	   tabela[1][0] == 'O' && tabela[1][1] == 'O' && tabela[1][2] == 'O' ||
 	   tabela[2][0] == 'O' && tabela[2][1] == 'O' && tabela[2][2] == 'O'){
-			cout << endl << "#---------- Jogador 1° Ganhou ----------#" << endl;
-			result = true;
-			exit(0);//Sair do Programa
-	}else 
+			cout << endl << "#---------- Jogador 1° " << player1 << " Ganhou ----------#" << endl;
+			exit(0); // Sair do Programa
+	}else
 
 	if(tabela[0][0] == 'O' && tabela[1][0] == 'O' && tabela[2][0] == 'O' ||
 	   tabela[0][1] == 'O' && tabela[1][1] == 'O' && tabela[2][1] == 'O' ||
 	   tabela[0][2] == 'O' && tabela[1][2] == 'O' && tabela[2][2] == 'O'){
-			 cout << endl << "#---------- Jogador 1° Ganhou ----------#" << endl;
-			 result = true;
-			 exit(0);//Sair do Programa
+			 cout << endl << "#---------- Jogador 1° " << player1 << " Ganhou ----------#" << endl;
+			 exit(0); //Sair do Programa
 	}else
 
 	if(tabela[0][0] == 'O' && tabela[1][1] == 'O' && tabela[2][2] == 'O' ||
 	   tabela[2][0] == 'O' && tabela[1][1] == 'O' && tabela[0][2] == 'O'){
-			cout << endl << "#---------- Jogador 1° Ganhou ----------#" << endl;
-			result = true;
-			exit(0);//Sair do Program
+			 cout << endl << "#---------- Jogador 1° " << player1 << " Ganhou ----------#" << endl;
+			exit(0); //Sair do Program
 	}
 
-	//Jogador 2
-	
+	// Verificando Jogadas do Segundo Jogador(a) 2
+
 	//Verifica a tabela no modo horizontal
-	if(tabela[0][0] == 'X' && tabela[0][1] == 'X' && tabela[0][2] == 'X' || 
+	if(tabela[0][0] == 'X' && tabela[0][1] == 'X' && tabela[0][2] == 'X' ||
 	   tabela[1][0] == 'X' && tabela[1][1] == 'X' && tabela[1][2] == 'X' ||
 	   tabela[2][0] == 'X' && tabela[2][1] == 'X' && tabela[2][2] == 'X'){
-			cout << endl << "#---------- Jogador 2° Ganhou ----------#" << endl;
-			result = true;
+			 cout << endl << "#---------- Jogador(a) 2° " << player2 << " Ganhou ----------#" << endl;
 			exit(0);//Sair do Programa
-	}else 
+	}else
 
 	if(tabela[0][0] == 'X' && tabela[1][0] == 'X' && tabela[2][0] == 'X' ||
 	   tabela[0][1] == 'X' && tabela[1][1] == 'X' && tabela[2][1] == 'X' ||
 	   tabela[0][2] == 'X' && tabela[1][2] == 'X' && tabela[2][2] == 'X'){
-			 cout << endl << "#---------- Jogador 2° Ganhou ----------#" << endl;
-			 result = true;
+			 cout << endl << "#---------- Jogador(a) 2° " << player2 << " Ganhou ----------#" << endl;
 			 exit(0);//Sair do Programa
 	}else
 
 	if(tabela[0][0] == 'X' && tabela[1][1] == 'X' && tabela[2][2] == 'X' ||
 	   tabela[2][0] == 'X' && tabela[1][1] == 'X' && tabela[0][2] == 'X'){
-			cout << endl << "#---------- Jogador 2° Ganhou ----------#" << endl;
-			result = true;
+			 cout << endl << "#---------- Jogador 2° " << player2 << " Ganhou ----------#" << endl;
 			exit(0);//Sair do Program
 	}else
 	//Verifica se tem empate
 	if(jogadas == 9){
 		cout << endl << "(: ---------- Jogo Empatado ---------- :) " << endl;
-		result = true;
+		//result = true;
 		exit(0);//Sair do Program
 	}
-	return result;
+	//return result;
 }//End Resultado
 
-
+// Função que Imprimi a tela do Jogo
 void tela(){
-	system("clear");//Limpando Tela [ Modifique para 'cls' no caso de Windows ]
-	cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;	
-	cout << "!!!!!! JOGO DA VELHA !!!!!!" << endl;	
-	cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
-	cout << endl;
-	//cout << "   0   1   2 " << endl;
-	cout << "     " << tabela[0][0] << " | " << tabela[0][1] << " | " << tabela[0][2] << endl;
-	cout << "    ---|---|--- " << endl;
-    cout << "     " << tabela[1][0] << " | " << tabela[1][1] << " | " << tabela[1][2] << endl;
-	cout << "    ---|---|--- " << endl;
-	cout << "     " << tabela[2][0] << " | " << tabela[2][1] << " | " << tabela[2][2] << endl;
-	
-	resultado();//Calculo das posições
 
-}//End Função Tela
+	menuBanner(); // Chamando a função do Banner do Software
+	int contJogadas = 9 - jogadas; // Verifica quantas jogadas foram realizadas
+	cout << setw(22) << "Faltam: " << contJogadas << " Jogadas" << endl << endl;
+	// Imprimindo a tela do Jogo
+	cout << setw(12) << tabela[0][0] << " | " << tabela[0][1] << " | " << tabela[0][2] << endl;
+	cout << setw(22) << "---|---|--- " << endl;
+  cout << setw(12) << tabela[1][0] << " | " << tabela[1][1] << " | " << tabela[1][2] << endl;
+	cout << setw(22) << "---|---|--- " << endl;
+	cout << setw(12) << tabela[2][0] << " | " << tabela[2][1] << " | " << tabela[2][2] << endl;
+
+	resultado(player1, player2); //Calculo das posições
+
+} //End Função Tela
 
 void condicao(int opcao, int joga1, int joga2){
 	if(joga1 < joga2){
@@ -126,7 +170,7 @@ void condicao(int opcao, int joga1, int joga2){
 					}
 					break;
 				//End Coluna 01
-				
+
 				//Coluna 02
 				case 4:
 					if(control[1][0] != 1){
@@ -166,7 +210,7 @@ void condicao(int opcao, int joga1, int joga2){
 						joga1--;
 						jogadas--;
 					}
-					break;	
+					break;
 				case 8:
 					if(control[2][1]  != 1){
 						tabela[2][1]  = 'X';
@@ -187,10 +231,11 @@ void condicao(int opcao, int joga1, int joga2){
 					break;
 				default:
 					cout << "Thau ! Opcao Invalida..." << endl;
-					i = 100;//Atribuição para terminar o programa
+					cout << "by: Walderlan Sena" << endl;
+					i = 99; //Atribuição para terminar o programa
 					break;
 				//End Coluna 03
-			}//Switch01
+			}//Switch 01
 		}else{
 			jogadas++;
 			switch(opcao){
@@ -223,7 +268,7 @@ void condicao(int opcao, int joga1, int joga2){
 					}
 					break;
 				//End Coluna 01
-				
+
 				//Coluna 02
 				case 4:
 					if(control[1][0] != 1){
@@ -263,7 +308,7 @@ void condicao(int opcao, int joga1, int joga2){
 						joga2--;
 						jogadas--;
 					}
-					break;	
+					break;
 				case 8:
 					if(control[2][1] != 1){
 						tabela[2][1]  = 'O';
@@ -290,34 +335,36 @@ void condicao(int opcao, int joga1, int joga2){
 			}//Switch02
 		}//End IF
 
-		
+
 }//End Função Condição
 
 //Metódo Main
 main(){
-										
-	int jogador1 = 1,jogador2 = 2;
-	
+
+	int jogador1 = 1, jogador2 = 2;
+	menuModo();
 	do{
-		int posicao;//Variavel de Controle		
-		tela();//Mostar a Tela Inicial do Jogo
-	
-		//Inicio do Jogo
-		
+		int posicao; //Variavel de Controle
+
+		tela();     //Mostar a Tela Inicial do Jogo
+
+		// ========== Inicio do Jogo =========
+
+		// Verifica qual jogador(a) deve realizar a jogada no momento
 		if(jogador1 < jogador2){
-			cout << endl << endl;
-			cout << "Primeiro Jogador: " << endl;
+			cout << endl << endl;;
+			cout << "Primeiro Jogador(a): " << player1 << endl << endl;
 			jogador1++;
 		}else{
 			cout << endl << endl;
-			cout << "Segundo Jogador: " << endl;
+			cout << "Segundo Jogador(a): " << player2  << endl << endl;
 			jogador2++;
 		}
-		//cout << "Total de Jogadas Realizadas: " << jogadas << endl;
-		cout << "Digite uma posicao: ";
-			cin >> posicao;
-		
-		condicao(posicao,jogador1,jogador2);//Verifica a Posição Digitada
+		cout << setw(27) << "Digite uma posicao: ";
+			cin >> posicao; // Captura o valor da posição digitada pelo o jogador(a)
 
-	}while(i != 100);
+		//Verifica a Posição Digitada
+		condicao(posicao,jogador1,jogador2);
+
+	}while(i != 99);
 }//End main
