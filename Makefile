13ce1cde60c7d4494efaf2d0c1da16ca70e29cd2
Walderlan Sena <senawalderlan@gmail.com>
2017-08-16 20:37:36 -0300

    Inicio de reorganização de todo código fonte. Adicionado funcionalidade de complilação via Makefile, geração de biblioteca compilada(Link dinâmico), e organização das função também em biblioteca estática

---
+++ Makefile
@@ -0,0 +1,15 @@
+# Compilador Ultilizado
+CC=g++
+# Localização da Biblioteca
+LIB=lib/velha.cpp
+# Nome do código do Jogo
+NAME=jogodavelha.cpp
+# Compilando todo o Software
+all:
+	$(CC) $(NAME) $(LIB) -o jogodavelha
+# Gerando Biblioteca Dinâmica para Sistemas Operacionais Linux
+libLinux:
+	$(CC) -shared -fPIC $(LIB) -o lib/libvelha.so
+# Excluido binário gerado
+clean:
+	rm -f jogodavelha lib/libvelha.so
---
+++ lib/velha.hpp
@@ -0,0 +1,53 @@
+#ifndef VELHA_H
+#define VELHA_H
+
+/**
+ * Inicio Bibliotecas ultilizadas no software
+ */
+
+#include <iostream>		//Biblioteca padrão c++
+#include <iomanip>		//|| Operação de espaços
+#include <cstdlib>		//|| Operação para limpar o teminal a cada impressão
+
+using namespace std;	// Usuando os espaço de nomes std
+
+/**
+ *	Prótotipo das função utilizadas no software
+ */
+
+/**
+ *	Função de demostração do nome e informações da versão do Jogo
+ */ 
+void vl_menuBanner(void);
+
+/**
+ *	Função de escolha para o modo do jogo - Single Player ou MultiPlayer
+ */ 
+void vl_menuModo(void);
+
+/**
+ *	Função que calcula o resultado o player1 e player2
+ *
+ */
+void vl_result(char player1[],char player2[]);
+
+/**
+ *	Função que imprime a tela inicial do jogo
+ */ 
+void vl_screen(void);
+
+
+/**
+ *	Verifica as codições para cada jogada
+ *	@param OPCAO (onde deseja Jogar)
+ *	@param joga1 (Quantidade de jogadas realizadas pelo Jogador1)
+ *	@param joga2 (Quantidade de jogadas realizadas pelo Jogador2)
+ */ 
+void vl_condicao(int opcao, int joga1, int joga2);
+
+/**
+ *	Função que inicializa o Game
+ */
+void vl_initGame(void);
+
+#endif
